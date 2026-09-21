34424281153c284b31ebf8d13feed527755de934
Roberto <zelayaroberto@rocketmail.com>
2026-09-21 06:07:04 -0600

    Remove unnecessary comment from app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 CORS(app)  # Me tienen podrido con el CORS, 
 #pero bueno, es lo que hay. No me da tiempo a hacer 
 #un proxy inverso ni nada de eso.
-
+#Igual ni funciono el despliegue ajajajajaja
 
 @app.route("/productos", methods=["GET"])
 def get_productos():
